54e120166432615030319edb2be9a3e255247fd0
Daniel Hast <32797673+HastD@users.noreply.github.com>
2026-06-09 17:26:23 -0400

    feat!: change ownership to linuxbrew user (#18)
    * feat!: change ownership to linuxbrew user
    
    Make the Homebrew installation owned by a dedicated `linuxbrew` user,
    created by systemd-sysusers. This creates a security boundary between
    the Homebrew installation and other unprivileged users.
    
    * feat: inline brew shellenv for bash and fish
    
    This should improve efficiency by reducing unnecesary calls to `brew`,
    especially when brew-proxy is in use.

--- etc/profile.d/brew.sh
+++ etc/profile.d/brew.sh
@@ -1,10 +1,22 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # SPDX-FileCopyrightText: Copyright 2025 Universal Blue
 # SPDX-FileCopyrightText: Copyright 2025 The BlueBuild Authors
+# SPDX-FileCopyrightText: Copyright 2026 Daniel Hast
 #
 # SPDX-License-Identifier: Apache-2.0
 
-if [[ -d /home/linuxbrew/.linuxbrew && $- == *i* && "$(/usr/bin/id -u)" != 0 ]]; then
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+if [ -x /home/linuxbrew/.linuxbrew/bin/brew ] && [ "$(/usr/bin/id -u)" != 0 ]; then
+  case "$-" in
+    *i*)
+      # Begin output of `brew shellenv bash` (lightly edited for formatting)
+      export HOMEBREW_PREFIX='/home/linuxbrew/.linuxbrew'
+      export HOMEBREW_CELLAR='/home/linuxbrew/.linuxbrew/Cellar'
+      export HOMEBREW_REPOSITORY='/home/linuxbrew/.linuxbrew/Homebrew'
+      export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin${PATH+:$PATH}";
+      [ -z "${MANPATH-}" ] || export MANPATH=":${MANPATH#:}"
+      export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:${INFOPATH:-}"
+      # End output of `brew shellenv bash`
+      ;;
+  esac
 fi
